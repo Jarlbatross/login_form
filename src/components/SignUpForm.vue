75480967d21fbdf9cf58432f37641542dcbfa060
Jarlbatross <jaraane@gmail.com>
2024-01-29 13:30:23 +0100

    Update SignUpForm.vue
    Added options and logic for adding skills

--- src/components/SignUpForm.vue
+++ src/components/SignUpForm.vue
@@ -1,13 +1,58 @@
 <template>
     <form>
         <label>Email:</label>
-        <input type="email" required>
+        <input type="email" required v-model="email">
+
+        <label>Password:</label>
+        <input type="password" required v-model="password">
+
+        <label>Role</label>
+        <select v-model = "role">
+        <option value="developer">Web Developer</option>
+        <option value="designer">Web Designer</option>
+        </select>
+
+        <label>Skills:</label>
+        <input type="text" v-model="tempSkill" @keyup="addSkill">
+        <div v-for="skill in skills" class="pill">
+            {{ skill }}
+        </div>
+
+        <div class="terms">
+        <input type="checkbox" v-model="terms" required>
+        <label>Accept Terms and Conditions</label>
+
+        </div>
+
+        
     </form>
+    <p>Email: {{email}}</p>
+    <p>Password: {{password}}</p>
+    <p>Role: {{role}}</p>
+    <p>Terms: {{terms}}</p>
 </template>
 
 <script>
-export default {
 
+export default {
+    data() {
+        return {
+            email: '',
+            password: '',
+            role: 'developer',
+            terms: false,
+            tempSkill: '',
+            skills: [],
+        }
+    },
+    methods: {
+        addSkill(e) {
+            if (e.key === ',' && this.tempSkill) {
+                this.skills.push(this.tempSkill);
+                this.tempSkill = '';
+            }
+        }
+    }
 }
 
 </script>
@@ -20,7 +65,7 @@ form {
     text-align: left;
     padding: 40px;
     border-radius: 10px;
-}
+} 
 label {
     color: #aaa;
     display: inline-block;
@@ -29,9 +74,9 @@ label {
     text-transform: uppercase;
     letter-spacing: 1px;
     font-weight: bold;
-}
-input {
-    display: block;
+} 
+input, select {
+    display: block; 
     padding: 10px 6px;
     width: 100%;
     box-sizing: border-box;
@@ -39,4 +84,12 @@ input {
     border-bottom: 1px solid #ddd;
     color: #555; 
 }
+input[type="checkbox"] {
+display: inline-block;
+width: 16px;
+margin: 0 10px 0 0;
+position: relative;
+top: 2px;
+}
+
 </style>
